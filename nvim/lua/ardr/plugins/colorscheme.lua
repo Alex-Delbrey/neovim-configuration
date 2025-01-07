a24e0a61734dc17bbb765cb148f27d17867d1545
Jandro <delbreyalex@gmail.com>
2025-01-07 18:59:57 -0400

    a more serious colorscheme

--- nvim/lua/ardr/plugins/colorscheme.lua
+++ nvim/lua/ardr/plugins/colorscheme.lua
@@ -1,16 +1,44 @@
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   config = function()
+--     require("tokyonight").setup({
+--       -- -- Make background transparent
+--       transparent = true,
+--       style = "night",
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--     })
+--     vim.cmd [[colorscheme tokyonight]]
+--   end
+-- }
+
 return {
-  "folke/tokyonight.nvim",
+  "ramojus/mellifluous.nvim",
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      -- -- Make background transparent
-      transparent = true,
-      style = "night",
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+    require("mellifluous").setup({
+      mellifluous = {
+        color_overrides = {
+          dark = {
+            bg = function(bg)
+              return bg:lightened(2)
+            end,
+            colors = function(colors)
+              return {
+                main_keywords = "#e39817",
+                operators = colors.functions:desaturated(10),
+              }
+            end,
+          },
+        },
       },
+      styles = {
+        main_keywords = { bold = true },
+      }
     })
-    vim.cmd [[colorscheme tokyonight]]
-  end
+    vim.cmd("colorscheme mellifluous")
+  end,
 }
