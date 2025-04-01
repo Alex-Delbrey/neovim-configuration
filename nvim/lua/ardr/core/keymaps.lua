f7f07a955c104c1933d83c026cf91ffa1eb8b966
Jandro <delbreyalex@gmail.com>
2025-03-31 22:19:47 -0400

    new keymap and stuff

--- nvim/lua/ardr/core/keymaps.lua
+++ nvim/lua/ardr/core/keymaps.lua
@@ -2,11 +2,13 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk"})
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+keymap.set("n", "<leader>err", "<CR>iif err != nil { log.Fatal(err) }<CR>", { desc = "log err in go" })
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>S", { desc = "Split window horizontally" })
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size"})
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
@@ -15,15 +17,14 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
-keymap.set("n", "<leader>nn", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer"})
-keymap.set("n", "<leader>nf", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file"})
-keymap.set("n", "<leader>nr", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer"})
+keymap.set("n", "<leader>nn", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+keymap.set("n", "<leader>nf", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file" })
+keymap.set("n", "<leader>nr", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy string in cwd" })
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Fuzzy string under cursor in cwd" })
 
-keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })             -- restore last workspace session for current directory
 keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
-
--- nvim/lua/ardr/plugins/colorscheme.lua
+++ nvim/lua/ardr/plugins/colorscheme.lua
@@ -1,46 +1,37 @@
--- return {
---   "folke/tokyonight.nvim",
---   priority = 1000,
---   config = function()
---     require("tokyonight").setup({
---       -- -- Make background transparent
---       transparent = true,
---       style = "night",
---       styles = {
---         sidebars = "transparent",
---         floats = "transparent",
---       },
---     })
---     vim.cmd [[colorscheme tokyonight]]
---   end
--- }
-
 return {
-  "ramojus/mellifluous.nvim",
+  "tinted-theming/tinted-vim",
   priority = 1000,
   config = function()
-    require("mellifluous").setup({
-      mellifluous = {
-        color_overrides = {
-          dark = {
-            bg = function(bg)
-              return bg:lightened(2)
-            end,
-            colors = function(colors)
-              return {
-                main_keywords = "#e39817",
-                strings = "#5db374",
-                operators = colors.functions:desaturated(10),
-                functions = "#5d82b3",
-              }
-            end,
-          },
-        },
-      },
-      styles = {
-        main_keywords = { bold = true },
-      }
-    })
-    vim.cmd("colorscheme mellifluous")
-  end,
+    vim.cmd.colorscheme('base16-black-metal')
+  end
 }
+
+-- return {
+--   "ramojus/mellifluous.nvim",
+--   priority = 1000,
+--   config = function()
+--     require("mellifluous").setup({
+--       mellifluous = {
+--         color_overrides = {
+--           dark = {
+--             bg = function(bg)
+--               return bg:lightened(2)
+--             end,
+--             colors = function(colors)
+--               return {
+--                 main_keywords = "#e39817",
+--                 strings = "#5db374",
+--                 operators = colors.functions:desaturated(10),
+--                 functions = "#5d82b3",
+--               }
+--             end,
+--           },
+--         },
+--       },
+--       styles = {
+--         main_keywords = { bold = true },
+--       }
+--     })
+--     vim.cmd("colorscheme mellifluous")
+--   end,
+-- }
